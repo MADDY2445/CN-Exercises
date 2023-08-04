b1e26d0d287be9486c1791f40c0284d97b9a6831
Madhanraj D <125901535+MADDY2445@users.noreply.github.com>
2023-08-04 14:57:15 +0530

    Add files via upload

---
+++ Tracce.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Tracce
+{
+public static void main(String[] arg)
+{
+try
+{
+Process proc = Runtime.getRuntime().exec("tracert www.yahoo.com");
+BufferedInputStream stream = new BufferedInputStream(proc.getInputStream());
+int data = 0;
+while((data = stream.read()) > 0)
+{
+System.out.print((char)data);
+}
+}
+catch (IOException e)
+{
+e.printStackTrace();
+}
+}
+}
